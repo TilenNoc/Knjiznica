1f062dbc0c1bea786d655a53663a30f5b92230cb
Eva Debenjak <debenjakeva33@gmail.com>
2016-12-22 18:14:52 +0100

    spletna

--- spletna.py
+++ spletna.py
@@ -9,9 +9,9 @@ def index():
 def pomoc():
     return 'Pojdi na stran /delitelji/xxx/ za delitelje števila xxx.'
 
-@route('/delitelji/<n>/')
-def delitelji(n):
-    n = int(n)
-    return template('seznam_deliteljev', n=n, delitelji=modeli.delitelji(n))
+##@route('/delitelji/<n>/')
+##def delitelji(n):
+##    n = int(n)
+##    return template('seznam_deliteljev', n=n, delitelji=modeli.delitelji(n))
 
 run(debug=True)
