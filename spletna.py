338fc3a970ef698925df20676e663050ef27a6d5
TilenNoc <tilen22noc@gmail.com>
2016-12-22 19:23:58 +0100

    Začel dodajati obrazce za urejanje

---
+++ spletna.py
@@ -0,0 +1,17 @@
+from bottle import route, run, template
+import modeli
+
+@route('/')
+def index():
+    return template('glavna')
+
+@route('/pomoc/')
+def pomoc():
+    return 'Pojdi na stran /delitelji/xxx/ za delitelje števila xxx.'
+
+@route('/delitelji/<n>/')
+def delitelji(n):
+    n = int(n)
+    return template('seznam_deliteljev', n=n, delitelji=modeli.delitelji(n))
+
+run(debug=True)
